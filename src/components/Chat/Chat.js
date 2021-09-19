c706620fb95c40fa1b87e0b73361778c250cab63
Adrian Zamora <adrianzamora10@outlook.com>
2021-09-18 19:06:31 -0600

    Chat header done

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Chat.css'
 import { Avatar, IconButton } from '@material-ui/core'
-import { AttachFile, SearchOutlined } from '@material-ui/icons'
+import { AttachFile, Message, SearchOutlined } from '@material-ui/icons'
 import MoreVert from '@material-ui/icons/MoreVert'
 
 const Chat = () => {
@@ -10,12 +10,12 @@ const Chat = () => {
             <div className="chat_header">
                 <Avatar />
 
-
                 <div className="chat_headerInfo">
                     <h3>Room name</h3>
                     <p>Last seen at...</p>
-
-                    <div className="chat__headerRight">
+                </div>
+                    <div 
+                    className="chat__headerRight">
                         <IconButton>
                             <SearchOutlined />
                         </IconButton>
@@ -25,8 +25,23 @@ const Chat = () => {
                         <IconButton>
                             <MoreVert />
                         </IconButton>
-                    </div>
                 </div>
+            </div>
+            <div className="chat__body">
+                <p className="chat__message">
+                    
+                    <span className="chat__name"> Adrian
+                    </span>
+                    
+                    This is a message
+
+                    <span className="chat__timestamp">
+                        {
+                        new Date().toUTCString()
+                        }
+                    </span>
+
+                </p>
             </div>
         </div>
     )
